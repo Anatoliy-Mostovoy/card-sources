d8823fb84708955ec08d5df1c86b68cfe8721074
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-10-02 15:52:07 +0300

    make plugin
    make plugin to choose active card

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const slides = document.querySelectorAll(".slide");
-console.log(slides);
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
+function slidesPlugin(activeSlides = 2) {
+  const slides = document.querySelectorAll(".slide");
+  slides[activeSlides].classList.add("active");
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
-}
+slidesPlugin();
